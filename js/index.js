06af77dfe67676c73603205bd28b9fa7e9dc9549
fagnersro <fagner.hg40@gmail.com>
2022-10-09 16:44:14 -0300

    Substituindo setTimeout por locationreload após alert de fim de jogo

--- js/index.js
+++ js/index.js
@@ -138,7 +138,7 @@ function iniciarJogo() {
       }
     } else {
       alert('Você atingiu o limíte de letras incorretas');
-      setTimeout(recarregar, 1000);
+      location.reload();
     }
   };
 }
